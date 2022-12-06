08269dd6fb7ee206ca7db80979f8bd4c1a815fce
Mathu Mounasamy <mmounasamy@gmail.com>
2022-12-06 01:48:21 +0100

    Add solution to day_05/part 1 of AoC 2022.

--- day_05/solution.py
+++ day_05/solution.py
@@ -1,6 +1,7 @@
 import pathlib
 import sys
-from typing import Any, List, Tuple
+from typing import Any, List, Tuple, Dict
+from io import StringIO
 
 import pandas as pd
 
@@ -24,6 +25,7 @@ def parse_input(puzzle_input: str) -> List[Any]:
 
     # Split the text into two section by empty line
     data: List[str] = [i.strip() for i in puzzle_input.split("\n\n")]
+    data: List[str] = puzzle_input.split("\n\n")
 
     return data
 
@@ -35,12 +37,40 @@ def solve_part_1(puzzle_input: str) -> int:
     """
 
     data: List[str] = parse_input(puzzle_input)
-    total_score: int = 0
-    # Calculate the total score
-    for i in data:
-        pass
+    # Pandas dataframe from string data
+    stacks_data: StringIO = StringIO(data[0])
+    procedures_data: StringIO = StringIO(data[1])
 
-    return total_score
+    stacks_df: pd.DataFrame = pd.read_fwf(stacks_data, colspecs="infer", header=None)
+    # set last row as column names
+    stacks_df = stacks_df.rename(columns=stacks_df.iloc[-1]).drop(stacks_df.index[-1])
+    # reverse the order of the rows
+    # stacks_df = stacks_df.iloc[::-1]
+    stacks_df = stacks_df[::-1]
+    # create dictionary of stacks
+    stacks_dict: Dict[str, List[str]] = stacks_df.to_dict(orient="list")
+    # remove pandas NaN values fom the dictionary values (lists)
+    for k, v in stacks_dict.items():
+        stacks_dict[k] = [i for i in v if not pd.isna(i)]
+
+    procedures_df: pd.DataFrame = pd.read_fwf(
+        procedures_data, colspecs="infer", header=None, names=["move", "from", "to"]
+    )
+
+    # Loop through the rows of the procedures dataframe
+    for index in procedures_df.index:
+        for i in range(procedures_df.loc[index, "move"]):
+            # Pop the last element from the "from" stack to append to the "to" stack
+            stacks_dict[str(procedures_df.loc[index, "to"])].append(
+                stacks_dict[str(procedures_df.loc[index, "from"])].pop()
+            )
+
+    # get the top element of each stack
+    message: str = ""
+    for col in stacks_df.columns:
+        message += stacks_dict[str(col)][-1]
+
+    return message
 
 
 def solve_part_2(puzzle_input: str) -> int:
@@ -48,7 +78,7 @@ def solve_part_2(puzzle_input: str) -> int:
     Solve part 2:
     """
 
-    data: List[Tuple[str, str]] = parse_input(puzzle_input)
+    data: List[str] = parse_input(puzzle_input)
     total_score: int = 0
     # Calculate the total score
     for i in data:
@@ -68,7 +98,8 @@ def solve_puzzle(puzzle_input):
 
 if __name__ == "__main__":
     # Read input data
-    puzzle_input = pathlib.Path(sys.argv[1]).read_text().strip()
+    # puzzle_input = pathlib.Path(sys.argv[1]).read_text().strip()
+    puzzle_input = pathlib.Path(sys.argv[1]).read_text()
 
     # Solve the puzzle
     solution_1, solution_2 = solve_puzzle(puzzle_input)
--- day_05/test_solution.py
+++ day_05/test_solution.py
@@ -9,22 +9,20 @@ PUZZLE_DIR = pathlib.Path(__file__).parent
 @pytest.fixture
 def example_data():
     """Example data."""
-    puzzle_input = (PUZZLE_DIR / "example.txt").read_text().strip()
+    puzzle_input = (PUZZLE_DIR / "example.txt").read_text()
 
     return puzzle_input
 
 
-# @pytest.mark.skip(reason="Not implemented yet")
 def test_parse_example(example_data):
     """Test parsing example."""
     parsed_date = sol.parse_input(example_data)
     assert parsed_date == [
-        "D     \n N   C     \n Z   M   P \n 1   2   3",
-        "1  2  1\n 3  1  3\n 2  2  1\n 1  1  2",
+        "     D     \n N   C     \n Z   M   P \n 1   2   3 ",
+        " 1  2  1\n 3  1  3\n 2  2  1\n 1  1  2",
     ]
 
 
-@pytest.mark.skip(reason="Not implemented yet")
 def test_part_1_example(example_data):
     """Test part 1 of the puzzle using the data example."""
     assert sol.solve_part_1(example_data) == "CMZ"
