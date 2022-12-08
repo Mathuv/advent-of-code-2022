a508c4cd64535339f1294073ad6625cbbcde4d2c
Mathu Mounasamy <mmounasamy@gmail.com>
2022-12-08 21:16:58 +0100

    AoC 2022/day_08/part_1: Implement parsing input text into numpy matrix.

--- day_08/solution.py
+++ day_08/solution.py
@@ -1,21 +1,30 @@
-from typing import List, Tuple, Any
-import pathlib
 import sys
+from pathlib import Path
+from typing import Any, List, Tuple
+
+import numpy as np
 
 
-def parse_input(puzzle_input: str) -> List[Any]:
+def parse_input(puzzle_input_file_path: Path) -> np.ndarray:
     """Parse input data."""
 
-    # Split the input into lines
-    lines: List[str] = puzzle_input.splitlines()
+    matrix: np.ndarray = np.empty(0, dtype=str)
+    with puzzle_input_file_path.open() as f:
+        # read line by line
+        for line in f:
+            # numpy array for the line
+            array_line: np.ndarray = np.array([list(line.strip())])
+            # concatinae each line's numpy array into matrix.
+            matrix = (
+                np.concatenate((matrix, array_line), axis=0)
+                if matrix.size > 0
+                else array_line
+            )
 
-    # Further process the lines
-    data: List[Any] = lines
-
-    return data
+    return matrix.astype(int)
 
 
-def solve_part_1(puzzle_input: str) -> int:
+def solve_part_1(puzzle_input_file_path: Path) -> int:
     """
     Solve part 1:
     """
@@ -29,7 +38,7 @@ def solve_part_1(puzzle_input: str) -> int:
     return total_score
 
 
-def solve_part_2(puzzle_input: str) -> int:
+def solve_part_2(puzzle_input_file_path: Path) -> int:
     """
     Solve part 2:
     """
@@ -43,18 +52,18 @@ def solve_part_2(puzzle_input: str) -> int:
     return total_score
 
 
-def solve_puzzle(puzzle_input):
+def solve_puzzle(puzzle_input_file_path: Path):
     """Solve the Advent of Code puzzle of the day."""
 
-    solution_1 = solve_part_1(puzzle_input)
-    solution_2 = solve_part_2(puzzle_input)
+    solution_1 = solve_part_1(puzzle_input_file_path)
+    solution_2 = solve_part_2(puzzle_input_file_path)
 
     return solution_1, solution_2
 
 
 if __name__ == "__main__":
     # Read input data
-    puzzle_input = pathlib.Path(sys.argv[1]).read_text().strip()
+    puzzle_input = Path(sys.argv[1]).read_text().strip()
 
     # Solve the puzzle
     solution_1, solution_2 = solve_puzzle(puzzle_input)
--- day_08/test_solution.py
+++ day_08/test_solution.py
@@ -1,5 +1,6 @@
 import pathlib
 
+import numpy as np
 import pytest
 import solution as sol
 
@@ -7,22 +8,30 @@ PUZZLE_DIR = pathlib.Path(__file__).parent
 
 
 @pytest.fixture
-def example_data():
+def example_data() -> pathlib.Path:
     """Example data."""
-    return (PUZZLE_DIR / "example.txt").read_text().strip()
+    return PUZZLE_DIR / "example.txt"
 
 
-@pytest.mark.skip(reason="Not implemented yet")
 def test_parse_example(example_data):
     """Test parsing example."""
-    parsed_date = sol.parse_input(example_data)
-    assert parsed_date == ...
+    parsed_data = sol.parse_input(example_data)
+    matrix: np.ndarray = np.array(
+        [
+            [3, 0, 3, 7, 3],
+            [2, 5, 5, 1, 2],
+            [6, 5, 3, 3, 2],
+            [3, 3, 5, 4, 9],
+            [3, 5, 3, 9, 0],
+        ]
+    )
+    assert np.array_equal(parsed_data, matrix)
 
 
 @pytest.mark.skip(reason="Not implemented yet")
 def test_part_1_example(example_data):
     """Test part 1 of the puzzle using the data example."""
-    assert sol.solve_part_1(example_data) == ...
+    assert sol.solve_part_1(example_data) == 21
 
 
 @pytest.mark.skip(reason="Not implemented yet")
