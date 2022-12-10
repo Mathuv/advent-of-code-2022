3b7bab0cf88c4c44eb7e85d200f96aba8224717b
Mathu Mounasamy <mmounasamy@gmail.com>
2022-12-10 19:47:54 +0100

    Update /day_template/ module.

--- day_template/solution.py
+++ day_template/solution.py
@@ -1,63 +1,49 @@
-from typing import List, Tuple, Any
-import pathlib
 import sys
+from pathlib import Path
+from typing import List
 
 
-def parse_input(puzzle_input: str) -> List[Any]:
+def parse_input(puzzle_input_file_path: Path) -> List[str]:
     """Parse input data."""
 
-    # Split the input into lines
-    lines: List[str] = puzzle_input.splitlines()
-
-    # Further process the lines
-    data: List[Any] = lines
-
-    return data
+    return puzzle_input_file_path.read_text().strip().splitlines()
 
 
-def solve_part_1(puzzle_input: str) -> int:
+def solve_part_1(puzzle_input_file_path: Path) -> int:
     """
     Solve part 1:
     """
 
-    data: List[Tuple[str, str]] = parse_input(puzzle_input)
-    total_score: int = 0
-    # Calculate the total score
-    for i in data:
-        pass
+    data: List[str] = parse_input(puzzle_input_file_path)
 
-    return total_score
+    return 0
 
 
-def solve_part_2(puzzle_input: str) -> int:
+def solve_part_2(puzzle_input_file_path: Path) -> int:
     """
     Solve part 2:
     """
 
-    data: List[Tuple[str, str]] = parse_input(puzzle_input)
-    total_score: int = 0
-    # Calculate the total score
-    for i in data:
-        pass
+    data: List[str] = parse_input(puzzle_input_file_path)
 
-    return total_score
+    return 0
 
 
-def solve_puzzle(puzzle_input):
+def solve_puzzle(puzzle_input_file_path: Path):
     """Solve the Advent of Code puzzle of the day."""
 
-    solution_1 = solve_part_1(puzzle_input)
-    solution_2 = solve_part_2(puzzle_input)
+    solution_1 = solve_part_1(puzzle_input_file_path)
+    solution_2 = solve_part_2(puzzle_input_file_path)
 
     return solution_1, solution_2
 
 
 if __name__ == "__main__":
     # Read input data
-    puzzle_input = pathlib.Path(sys.argv[1]).read_text().strip()
+    puzzle_input_file_path: Path = Path(sys.argv[1])
 
     # Solve the puzzle
-    solution_1, solution_2 = solve_puzzle(puzzle_input)
+    solution_1, solution_2 = solve_puzzle(puzzle_input_file_path)
 
     # Print the solutions
     print(f"Solution 1: {solution_1}")
--- day_template/test_solution.py
+++ day_template/test_solution.py
@@ -7,27 +7,25 @@ PUZZLE_DIR = pathlib.Path(__file__).parent
 
 
 @pytest.fixture
-def example_data():
+def example_data() -> pathlib.Path:
     """Example data."""
-    puzzle_input = (PUZZLE_DIR / "example.txt").read_text().strip()
-
-    return puzzle_input
+    return PUZZLE_DIR / "example.txt"
 
 
-@pytest.mark.skip(reason="Not implemented yet")
+@pytest.mark.skip("Not implemented yet")
 def test_parse_example(example_data):
     """Test parsing example."""
-    parsed_date = sol.parse_input(example_data)
-    assert parsed_date == ...
+    parsed_data = sol.parse_input(example_data)
+    assert parsed_data == ...
 
 
-@pytest.mark.skip(reason="Not implemented yet")
+@pytest.mark.skip("Not implemented yet")
 def test_part_1_example(example_data):
     """Test part 1 of the puzzle using the data example."""
     assert sol.solve_part_1(example_data) == ...
 
 
-@pytest.mark.skip(reason="Not implemented yet")
+@pytest.mark.skip("Not implemented yet")
 def test_part_2_example(example_data):
     """Test part 2 of the puzzle using the data example."""
     assert sol.solve_part_2(example_data) == ...
