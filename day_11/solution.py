81ad7ac3367b107eb723d5954a11cc240e97deec
Mathu Mounasamy <mmounasamy@gmail.com>
2022-12-12 11:38:58 +0100

    Implement solution for day_11/part_2 of the puzzle (AoC 2022).

--- day_11/solution.py
+++ day_11/solution.py
@@ -2,7 +2,6 @@ import sys
 import math
 from pathlib import Path
 from typing import Any, Dict, List
-from pprint import pprint
 
 import yaml
 
@@ -35,45 +34,34 @@ def parse_input(puzzle_input_file_path: Path) -> Dict[str, Any]:
     return document
 
 
-def calc_level_of_monkey_business(document: Dict[str, Any], rounds: int = 20) -> int:
+def calc_level_of_monkey_business(
+    document: Dict[str, Any], rounds: int = 20, divide_worry_level: bool = True
+) -> int:
     # Initialize 'inspected=0' for all monkeys
     for value in document.values():
         value["inspected"] = 0
 
-    pprint(document)
     for i in range(rounds):
-        pprint(f"round: {i}")
         for monkey, v in document.items():
-            print(f"monkey: {monkey}")
             starting_items: List[int] = v["starting items"]
             while len(starting_items):
                 worry = starting_items.pop(0)  # not a good practice
-                print(f"worry initial: {worry}")
                 v["inspected"] += 1
                 operation: str = v["operation"]
-                print(f"operation: {operation}")
-                # exec(operation, {"worry": worry})
                 worry = eval(operation)
-                print(f"worry after operation: {worry}")
-                # worry = round(worry / 3)
-                worry = worry // 3
-                print(f"worry after /3: {worry}")
-                print(f"test: {v['test']}")
+                if divide_worry_level:
+                    worry = worry // 3
                 if worry % v["test"] == 0:  # Divisible
                     target_monkey = v[True]
                 else:
                     target_monkey = v[False]
-                print(f"target monkey: {target_monkey}")
                 target_starting_items: List[int] = document[target_monkey][
                     "starting items"
                 ]
-                print(f"target_starting_items: {target_starting_items}")
                 target_starting_items.append(worry)
-                pprint(document)
 
     inspected_items_list = [v["inspected"] for v in document.values()]
     inspected_items_list.sort(reverse=True)
-    print(f"inspected items: {inspected_items_list}")
 
     return math.prod(inspected_items_list[:2])
 
@@ -94,9 +82,11 @@ def solve_part_2(puzzle_input_file_path: Path) -> int:
     Solve part 2:
     """
 
-    document: str = parse_input(puzzle_input_file_path)
+    document: Dict[str, Any] = parse_input(puzzle_input_file_path)
 
-    return 0
+    return calc_level_of_monkey_business(
+        document=document, rounds=10000, divide_worry_level=False
+    )
 
 
 def solve_puzzle(puzzle_input_file_path: Path):
