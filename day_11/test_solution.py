2718eae47bfb29bbf63de5fb456741a642b9acdb
Mathu Mounasamy <mmounasamy@gmail.com>
2022-12-12 02:25:14 +0100

    Implement solution for day_11/part_1 of the puzzle AoC 2022.

--- day_11/test_solution.py
+++ day_11/test_solution.py
@@ -16,40 +16,40 @@ def example_data() -> pathlib.Path:
 def test_parse_example(example_data):
     """Test parsing example."""
     parsed_data = sol.parse_input(example_data)
-    # pprint(parsed_data)
+    pprint(parsed_data)
     assert parsed_data == {
-        "monkey 0": {
-            "if false": 3,
-            "if true": 2,
-            "operation": "new = old * 19",
-            "starting items": "79, 98",
+        0: {
+            False: 3,
+            True: 2,
+            "operation": "worry * 19",
+            "starting items": [79, 98],
             "test": 23,
         },
-        "monkey 1": {
-            "if false": 0,
-            "if true": 2,
-            "operation": "new = old + 6",
-            "starting items": "54, 65, 75, 74",
+        1: {
+            False: 0,
+            True: 2,
+            "operation": "worry + 6",
+            "starting items": [54, 65, 75, 74],
             "test": 19,
         },
-        "monkey 2": {
-            "if false": 3,
-            "if true": 1,
-            "operation": "new = old * old",
-            "starting items": "79, 60, 97",
+        2: {
+            False: 3,
+            True: 1,
+            "operation": "worry * worry",
+            "starting items": [79, 60, 97],
             "test": 13,
         },
-        "monkey 3": {
-            "if false": 1,
-            "if true": 0,
-            "operation": "new = old + 3",
-            "starting items": 74,
+        3: {
+            False: 1,
+            True: 0,
+            "operation": "worry + 3",
+            "starting items": [74],
             "test": 17,
         },
     }
 
 
-@pytest.mark.skip("Not implemented yet")
+# @pytest.mark.skip("Not implemented yet")
 def test_part_1_example(example_data):
     """Test part 1 of the puzzle using the data example."""
     assert sol.solve_part_1(example_data) == 10605
