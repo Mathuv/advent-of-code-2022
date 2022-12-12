21754f5c12b8f17e6933308bbea0ad9986dc7967
Mathu Mounasamy <mmounasamy@gmail.com>
2022-12-12 19:40:55 +0100

    AOC2022/day_12: Implement function 'parse_input' and unit test.

--- day_12/solution.py
+++ day_12/solution.py
@@ -1,12 +1,16 @@
 import sys
 from pathlib import Path
 from typing import List
+import numpy as np
 
 
-def parse_input(puzzle_input_file_path: Path) -> List[str]:
+def parse_input(puzzle_input_file_path: Path) -> np.ndarray:
     """Parse input data."""
 
-    return puzzle_input_file_path.read_text().strip().splitlines()
+    text: str = puzzle_input_file_path.read_text().strip()
+
+    # Convert the text into numpy array and return
+    return np.array([list(line) for line in text.splitlines()])
 
 
 def solve_part_1(puzzle_input_file_path: Path) -> int:
@@ -14,7 +18,7 @@ def solve_part_1(puzzle_input_file_path: Path) -> int:
     Solve part 1:
     """
 
-    data: List[str] = parse_input(puzzle_input_file_path)
+    grid: np.ndarray = parse_input(puzzle_input_file_path)
 
     return 0
 
@@ -24,13 +28,16 @@ def solve_part_2(puzzle_input_file_path: Path) -> int:
     Solve part 2:
     """
 
-    data: List[str] = parse_input(puzzle_input_file_path)
+    grid: np.ndarray = parse_input(puzzle_input_file_path)
 
     return 0
 
 
 def solve_puzzle(puzzle_input_file_path: Path):
-    """Solve the Advent of Code puzzle of the day."""
+    """
+    Solve the Advent of Code puzzle of the day.
+    Day 12: Hill Climbing Algorithm
+    """
 
     solution_1 = solve_part_1(puzzle_input_file_path)
     solution_2 = solve_part_2(puzzle_input_file_path)
--- day_12/test_solution.py
+++ day_12/test_solution.py
@@ -1,7 +1,11 @@
 import pathlib
 
+import numpy as np
 import pytest
 import solution as sol
+
+# from pprint import pprint
+
 
 PUZZLE_DIR = pathlib.Path(__file__).parent
 
@@ -12,17 +16,27 @@ def example_data() -> pathlib.Path:
     return PUZZLE_DIR / "example.txt"
 
 
-@pytest.mark.skip("Not implemented yet")
+# @pytest.mark.skip("Not implemented yet")
 def test_parse_example(example_data):
     """Test parsing example."""
+    grid: np.ndarray = np.array(
+        [
+            ["S", "a", "b", "q", "p", "o", "n", "m"],
+            ["a", "b", "c", "r", "y", "x", "x", "l"],
+            ["a", "c", "c", "s", "z", "E", "x", "k"],
+            ["a", "c", "c", "t", "u", "v", "w", "j"],
+            ["a", "b", "d", "e", "f", "g", "h", "i"],
+        ]
+    )
     parsed_data = sol.parse_input(example_data)
-    assert parsed_data == ...
+    # pprint(parsed_data)
+    assert np.array_equal(parsed_data, grid)
 
 
 @pytest.mark.skip("Not implemented yet")
 def test_part_1_example(example_data):
     """Test part 1 of the puzzle using the data example."""
-    assert sol.solve_part_1(example_data) == ...
+    assert sol.solve_part_1(example_data) == 31
 
 
 @pytest.mark.skip("Not implemented yet")
