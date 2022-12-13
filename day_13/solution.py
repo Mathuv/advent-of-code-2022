b99be1bca22e59c8ad702f9ccfb6434f97987f1d
Mathu Mounasamy <mmounasamy@gmail.com>
2022-12-13 13:23:29 +0100

    Modify 'def parse_input' to return list of tuples to represent pairs of packets.

--- day_13/solution.py
+++ day_13/solution.py
@@ -1,24 +1,30 @@
 import sys
 from pathlib import Path
-from typing import List, Any
+from typing import Any, List, Tuple
 
 
-def parse_input(puzzle_input_file_path: Path) -> List[List[Any]]:
-    """Parse input data."""
+def parse_input(puzzle_input_file_path: Path) -> List[Tuple[List[Any], List[Any]]]:
+    """
+    Parse input data.
+
+    Returns: Pairs of packets (list of tuples)
+    """
 
     text: str = puzzle_input_file_path.read_text().strip()
     # replace '\n\n' with '|' and '\n' with ','
     text = text.replace("\n\n", "|").replace("\n", ",")
     pairs = text.split("|")
-    return [list(eval(x)) for x in pairs]
+    return [eval(x) for x in pairs]
 
 
 def solve_part_1(puzzle_input_file_path: Path) -> int:
     """
     Solve part 1:
+    Determine which pairs of packets are already in the right order.
+    What is the sum of the indices of those pairs?
     """
 
-    data: List[str] = parse_input(puzzle_input_file_path)
+    packet_pairs: List[Tuple[List[Any], List[Any]]] = parse_input(puzzle_input_file_path)
 
     return 0
 
@@ -28,13 +34,16 @@ def solve_part_2(puzzle_input_file_path: Path) -> int:
     Solve part 2:
     """
 
-    data: List[str] = parse_input(puzzle_input_file_path)
+    packet_pairs: List[Tuple[List[Any], List[Any]]] = parse_input(puzzle_input_file_path)
 
     return 0
 
 
 def solve_puzzle(puzzle_input_file_path: Path):
-    """Solve the Advent of Code puzzle of the day."""
+    """
+    Solve the Advent of Code puzzle of the day.
+    Day 13: Distress Signal
+    """
 
     solution_1 = solve_part_1(puzzle_input_file_path)
     solution_2 = solve_part_2(puzzle_input_file_path)
--- day_13/test_solution.py
+++ day_13/test_solution.py
@@ -16,18 +16,17 @@ def test_parse_example(example_data):
     """Test parsing example."""
     parsed_data = sol.parse_input(example_data)
     assert parsed_data == [
-        [[1, 1, 3, 1, 1], [1, 1, 5, 1, 1]],
-        [[[1], [2, 3, 4]], [[1], 4]],
-        [[9], [[8, 7, 6]]],
-        [[[4, 4], 4, 4], [[4, 4], 4, 4, 4]],
-        [[7, 7, 7, 7], [7, 7, 7]],
-        [[], [3]],
-        [[[[]]], [[]]],
-        [[1, [2, [3, [4, [5, 6, 7]]]], 8, 9], [1, [2, [3, [4, [5, 6, 0]]]], 8, 9]],
+        ([1, 1, 3, 1, 1], [1, 1, 5, 1, 1]),
+        ([[1], [2, 3, 4]], [[1], 4]),
+        ([9], [[8, 7, 6]]),
+        ([[4, 4], 4, 4], [[4, 4], 4, 4, 4]),
+        ([7, 7, 7, 7], [7, 7, 7]),
+        ([], [3]),
+        ([[[]]], [[]]),
+        ([1, [2, [3, [4, [5, 6, 7]]]], 8, 9], [1, [2, [3, [4, [5, 6, 0]]]], 8, 9]),
     ]
 
 
-@pytest.mark.skip("Not implemented yet")
 def test_part_1_example(example_data):
     """Test part 1 of the puzzle using the data example."""
     assert sol.solve_part_1(example_data) == 13
