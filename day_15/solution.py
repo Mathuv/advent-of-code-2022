b45740420231b4d579863f62cef8fa4ed524827f
Mathu Mounasamy <mmounasamy@gmail.com>
2022-12-17 18:17:25 +0100

    Implement 'def create_grid' for day_15/part_1 of AoC 2022.

--- day_15/solution.py
+++ day_15/solution.py
@@ -1,9 +1,13 @@
 import sys
 from pathlib import Path
-from typing import List
+from pprint import pprint
+from typing import List, Tuple
+
+import numpy as np
+from icecream import ic
 
 
-def parse_input(puzzle_input_file_path: Path) -> List[str]:
+def parse_input(puzzle_input_file_path: Path) -> List[List[Tuple[int, int]]]:
     """Parse input data."""
 
     text: str = puzzle_input_file_path.read_text().strip()
@@ -13,21 +17,63 @@ def parse_input(puzzle_input_file_path: Path) -> List[str]:
         .replace("y=", "")
         .replace(" closest beacon is at x=", "")
     )
-    positions = [[eval(pos) for pos in line.split(":")] for line in text.splitlines()]
+    positions = [
+        [(eval(pos)[1], eval(pos)[0]) for pos in line.split(":")]
+        for line in text.splitlines()
+    ]
 
     return positions
 
 
-def solve_part_1(puzzle_input_file_path: Path) -> int:
+def create_grid(
+    positions: List[List[Tuple[int, int]]], floor_at_bottom: bool = False
+) -> np.ndarray:
+    """
+    Crate just enough size of the numpy array to accomodate cordinates in the scans
+    """
+
+    # find min_row, max_row and min_col and max_col from scans for numpy array
+    min_row = 0
+    max_row = max(max(c[0] for c in position) for position in positions)
+    min_col = min(min(c[1] for c in position) for position in positions)
+    max_col = max(max(c[1] for c in position) for position in positions)
+
+    rows = (max_row - min_row) + 1
+    cols = (max_col - min_col) + 1
+
+    # Draw the grid of air
+    grid: np.ndarray = np.full((rows, cols), ".")
+    ic(grid.shape)
+    ic("\n", grid)
+
+    return grid
+
+
+def find_positions_cannot_contain_beacons(
+    positions: List[List[Tuple[int, int]]], row: int
+) -> int:
+    """
+    Find the positions that cannot contain a beacon.
+    """
+
+    grid: np.ndarray = create_grid(positions, floor_at_bottom=True)
+
+    # Find the positions that cannot contain a beacon
+    positions_cannot_contain_beacons: int = 0
+
+    return positions_cannot_contain_beacons
+
+
+def solve_part_1(puzzle_input_file_path: Path, row: int = 2000000) -> int:
     """
     Solve part 1:
     Consult the report from the sensors you just deployed.
     In the row where y=2000000, how many positions cannot contain a beacon?
     """
 
-    data: List[str] = parse_input(puzzle_input_file_path)
+    positions: List[List[Tuple[int, int]]] = parse_input(puzzle_input_file_path)
 
-    return 0
+    return find_positions_cannot_contain_beacons(positions, row)
 
 
 def solve_part_2(puzzle_input_file_path: Path) -> int:
